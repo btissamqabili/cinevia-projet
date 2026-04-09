9833c70f82c53f240f64a5ff31b3e5c41b841341
btissamqabili <qabilibtissam8@gmail.com>
2026-04-09 16:56:30 +0100

    modif

--- index.js
+++ index.js
@@ -1,73 +1,134 @@
 const films=[{name:"no rules,no fear,just impact",
      category: "action",
     description:"Film d'action intense sur un combattant sans limites qui affronte des ennemis dangereux dans un monde sans règles, où seule la force fait la différence",
-    image:"assets (images)/imgc1.png.jpg" },
+    image:"images/imgc1.png" },
     {name: "La vie pour de vrai",
         category: "comédie",
     description: "Un homme découvre le monde réel après avoir vécu isolé toute sa vie.",
-     image:"assets (images)/imgc2.png.jpg"  },
-     {name: "Jumanji: The Next Level",
-     category: "action",
-    description: "Les joueurs retournent dans un jeu dangereux pour sauver leur ami.",
-image: "assets (images)/img4.png.jpg"},
- {name:"Jumanji 4", 
+     image:"images/imgc2.png"  },
+     {
+          name:"Jumanji 4", 
      category: "action",
      description: "Suite attendue de la saga Jumanji où les joueurs retournent dans un univers encore plus dangereux avec de nouveaux défis.",
-    image:"assets (images)/img3.png.jpg"
+     image: "images/img3.png"},
+ {name: "Rambo 6:the last stand",
+     category: "action",
+    description: "Les joueurs retournent dans un jeu dangereux pour sauver leur ami.",
+
+    image:"images/img4.png"
  },
-  {name:"Rambo 6 The last stand",
-     category: "action", 
-    description: "Rambo sort de sa retraite pour sauver une jeune fille kidnappée par un cartel, déclenchant une vengeance violente.",
-    image:"assets (images)/img4.png.jpg" },
 {name:"Bienvenue  à bord",
      category: "comédie",
     description:"Comédie sur une croisière remplie de situations absurdes et de personnages hauts en couleur.",
-    image:"assets (images)/img5.png.jpg" },
+    image:"images/img5.png" },
 {name:"The Expendables 3",
      category: "action",
      description:"Une équipe de mercenaires affronte un ancien membre devenu ennemi.",
-    image:"assets (images)/imgc6.png.jpg" },
+    image:"images/imgc6.png" },
     {name:"FREE  FALL",
          category: "drame",
          description:"Un policier tombe amoureux de son collègue, mettant sa vie personnelle en danger.",
-    image:"assets (images)/imgc7.png.jpg" },
+    image:"images/imgc7.png" },
     {name:"Ducobu 3",
          category: "comédie",
          description:"L’élève tricheur revient avec de nouvelles stratégies pour éviter de travailler.",
-    image:"assets (images)/imgc8.png.jpg" },
+    image:"images/imgc8.png" },
     {name:"Before Night Falls",
          category: "drame",
          description:"Biographie d’un écrivain cubain confronté à la répression politique.",
-    image:"assets (images)/imgc9.png.jpg" },
+    image:"images/imgc9.png" },
      {name:"the train of love",
          category: "drame",
          description:"Drame romantique sur deux inconnus qui se rencontrent dans un train, où leur voyage devient une histoire d’amour inattendue entre émotions, choix et destin",
-    image:"assets (images)/imgc10.png.jpg" },
+    image:"images/imgc10.png" },
      {name:"Extraction 3",
          category: "action",
          description:"Suite d’action centrée sur des missions de sauvetage à haut risque",
-    image:"assets (images)/imgc11.png.jpg" },
+    image:"images/imgc11.png" },
      {name:"un ticket pour deux",
          category: "comédie",
          description:"Deux hommes opposés voyagent ensemble dans une aventure pleine de conflits et d’humour.",
-    image:"assets (images)/imgc12.png.jpg" },
+    image:"images/imgc12.png" },
      {name:"Inception",
          category: "action",
          description:"Des voleurs infiltrent les rêves pour implanter une idée dans l’esprit d’une cible",
-    image:"assets (images)/img13.png.jpg" },
+    image:"images/img13.png" },
      {name:" Fast & Furious 9",
          category: "action",
          description:"Dom affronte son frère dans une mission liée à un complot mondial.",
-    image:"assets (images)/img14.png.jpg" },
+    image:"images/img14.png" },
      {name:" Mission: Impossible-Fallout",
          category: "action",
          description:"Ethan Hunt doit empêcher une catastrophe nucléaire après une mission échouée.",
-    image:"assets (images)/img15.png.jpg" },
+    image:"images/img15.png" },
      {name:"Gladiator",
          category: "action",
          description:"Un général romain trahi devient gladiateur pour se venger de l’empereur.",
-    image:"assets (images)/img16.png.jpg" },
+    image:"images/img16.png" },
 ]
-const container=document.getElementById("movie-container")
+
+const container = document.getElementById("movie-container");
+
+films.forEach((film) => {
+  const card = createCard(film);
+  container.appendChild(card);
+});
+function createCard(film) {
+
+  const card = document.createElement("div");
+  card.classList.add("card");
 
 
+  const img = document.createElement("img");
+  img.src = film.image;
+  img.alt = film.name;
+
+  const title = document.createElement("h3");
+  title.textContent = film.name;
+
+  const desc = document.createElement("p");
+  desc.textContent = film.description;
+  desc.classList.add("desc")
+  const category = document.createElement("span");
+  category.textContent = film.category;
+  category.classList.add("category");
+
+  const btn = document.createElement("button");
+  btn.textContent = "Voir plus";
+
+  btn.addEventListener("click", () => {
+  modal.style.display = "flex";
+
+  modalImg.src = film.image;
+  modalTitle.textContent = film.name;
+  modalDesc.textContent = film.description;
+
+  currentFilm = film;
+});
+
+  card.appendChild(img);
+  card.appendChild(title);
+  card.appendChild(category);
+  card.appendChild(desc);
+  card.appendChild(btn);
+
+  return card;
+}
+const modal = document.getElementById("modal");
+const modalImg = document.getElementById("modal-img");
+const modalTitle = document.getElementById("modal-title");
+const modalDesc = document.getElementById("modal-desc");
+const closeBtn = document.querySelector(".close");
+const addFavBtn = document.getElementById("add-fav");
+
+let currentFilm = null;
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+const favorites = [];
+
+addFavBtn.addEventListener("click", () => {
+  favorites.push(currentFilm);
+  alert("Ajouté aux favoris !");
+  modal.style.display = "none";
+});
